d8fde4ebd5e5293fb27849e2d0cfe76388cc77d1
olusesimrg <olusesimrgees@gmail.com>
2018-09-13 17:21:47 +0100

    modified js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-
 //addin line through and changin the color to gray
-$("li").click( function() {
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("connect");
 });
 
-//creating the delate button
+$("input[type='text']").keypress(function(e){
+	if(e.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+//creating a new li and add to the ul
+		$("ul").append('<li><span>x</span>' + todoText + '</li>');
+	}
+});
 
-$("span").click( function(event){
+// creating the delate button
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
 
-$("input[type='text']").keypress(function(e) {
-	if(e.which === 13){
-		var todoText = $(this).val();
-		$(this).val("");
-//creating a new li and add to the ul
-		$("ul").append("<li><span>X</span>" + todoText + "</li>");
-	}
-});
+
 //hiding and showing the text input
 $(".plus").click(function(){
-	$("input[type='text']").fadeToggle(500);
+	$("input[type='text']").fadeToggle(700);
 });
